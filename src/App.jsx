fada69ec0e1fd0bc3f771a898ea06ca12682dd06
Adrian Doonan <adriandoonan@gmail.com>
2024-03-14 09:30:39 +0100

    include the files

---
+++ src/App.jsx
@@ -0,0 +1,66 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <>
+      <header>
+        <nav id="main-nav">
+          <ul>
+            <li>
+              <strong>Acme Corp</strong>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Services</a>
+            </li>
+            <li>
+              <a href="#">Products</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <section id="main-content">
+        <aside id="sidebar">
+          {" "}
+          <nav>
+            <ul>
+              <li>
+                <a href="#">About</a>
+              </li>
+              <li>
+                <a href="#">Services</a>
+              </li>
+              <li>
+                <a href="#">Products</a>
+              </li>
+            </ul>
+          </nav>
+        </aside>
+        <main className="container">main page</main>
+        <div></div>
+      </section>
+      <footer>
+        <nav id="footer-nav">
+          {" "}
+          <ul>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Services</a>
+            </li>
+            <li>
+              <a href="#">Products</a>
+            </li>
+          </ul>
+        </nav>
+      </footer>
+    </>
+  );
+}
+
+export default App;
