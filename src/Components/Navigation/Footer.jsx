d539514a68617ef64ccd9a9eeb4a9bfdc690455a
mmisurell <m.misurell@gmail.com>
2024-03-20 17:55:44 +0100

    updates: about page + footer

--- src/Components/Navigation/Footer.jsx
+++ src/Components/Navigation/Footer.jsx
@@ -1,29 +1,29 @@
 import { Link } from "react-router-dom";
 
 const Footer = () => {
-	return (
-		<footer>
-			<nav id="footer-nav">
-				<ul>
-					<li>
-						<Link to="/about">About</Link>
-					</li>
-					<li>
-						<Link to="/events">Events</Link>
-					</li>
-					<li>
-						<Link to="/events-calendar">Calendar</Link>
-					</li>
-					<li>
-						<Link to="/impressum">Impressum</Link>
-					</li>
-				</ul>
-				<small>
-					Copyright&nbsp;©&nbsp;{new Date().getFullYear()}
-					&nbsp;Eventz&nbsp;Cooperative
-				</small>
-			</nav>
-		</footer>
-	);
+  return (
+    <footer>
+      <nav id="footer-nav">
+        <ul>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/events">Events</Link>
+          </li>
+          <li>
+            <Link to="/events-calendar">Calendar</Link>
+          </li>
+          <li>
+            <Link to="/impressum">Impressum</Link>
+          </li>
+        </ul>
+        <small>
+          Copyright&nbsp;©&nbsp;{new Date().getFullYear()}{" "}
+          &nbsp;Eventz&nbsp;Cooperative
+        </small>
+      </nav>
+    </footer>
+  );
 };
 export default Footer;
